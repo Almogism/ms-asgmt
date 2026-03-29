30b23952f412f6e8ab2fc9b46ff4419fad1ac868
Almog Bokobza <almogishot@gmail.com>
2026-03-29 12:56:39 +0300

    add create service principal command

--- etc/random.sh
+++ etc/random.sh
@@ -1,2 +1,9 @@
 # Check if traffic is allowed
 kubectl exec -it <pod> -- bash -c "timeout 5 bash -c 'echo > /dev/tcp/<endpoint_ip>/<endpoint_port>' && echo OPEN || echo BLOCKED"
+
+# Create service principal (similar to service account)
+az ad sp create-for-rbac \
+  --name "github-actions-acr" \
+  --role contributor \
+  --scopes /subscriptions/<YOUR_SUBSCRIPTION_ID>/resourceGroups/<YOUR_RESOURCE_GROUP> \
+  --sdk-auth
